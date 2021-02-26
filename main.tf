e67a412769dab642126cba1fa5f00362c8b39add
Amanibhavam <iam@defn.sh>
2021-02-25 23:03:13 -0800

    correct version of terraform-aws-ec2-auto-scale-group

--- main.tf
+++ main.tf
@@ -170,7 +170,7 @@ data "aws_iam_instance_profile" "default" {
 }
 
 module "autoscale_group" {
-  source = "git::https://github.com/scalyr/terraform-aws-ec2-autoscale-group.git?ref=0.16.0-scalyr2"
+  source = "git::https://github.com/scalyr/terraform-aws-ec2-autoscale-group.git?ref=0.4.0-scalyr2"
 
   enabled    = var.enabled
   namespace  = var.namespace
